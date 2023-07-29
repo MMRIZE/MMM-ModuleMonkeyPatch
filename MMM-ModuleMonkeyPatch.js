7b722e0431a8fe1f1470bbe4928825d91dd0c812
eouia <eouia0819@gmail.com>
2023-07-29 16:57:43 +0200

    release

---
+++ MMM-ModuleMonkeyPatch.js
@@ -0,0 +1,48 @@
+Module.register("MMM-ModuleMonkeyPatch", {
+  defaults: {
+    patches: [
+      {
+        module: "calendar",
+        method: "getDom",
+        patch: function (original, args) {
+          const ret = original(args)
+          // do something
+          return ret
+        }
+      },
+    ]
+  },
+
+  start: function () {
+    this.originals = new WeakMap()
+  },
+
+  notificationReceived: function (notification, payload, sender) {
+    if (notification === "ALL_MODULES_STARTED") {
+      this.patchModules()
+    }
+  },
+
+  patchModules: function () {
+    const modules = MM.getModules()
+    for (let module of modules) {
+      for (let patch of this.config.patches) {
+        if (
+          module
+          && module.name === patch.module
+          && typeof module[ patch.method ] === "function"
+          && typeof patch.patch === "function"
+        ) {
+          Log.log(`MONKEYPATCH: ${patch.module}.${patch.method}`)
+          const original = module[ patch.method ]
+          //this.originals.set(original, original)
+          module[ patch.method ] = new Proxy(original, {
+            apply: function (target, thisArg, args) {
+              return patch.patch.bind(module)(original.bind(module), args)
+            }
+          })
+        }
+      }
+    }
+  }
+})
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # MMM-ModuleMonkeyPatch
 MagicMirror module to make monkeypatching other module.
+
+## Installation
+```sh
+cd <MAGICMIRROR DIRECTORY>/modules
+git clone https://github.com/MMRIZE/MMM-ModuleMonkeyPatch
+```
+
+## Configuration
+```js
+{
+  module: "MMM-ModuleMonkeyPatch",
+  config: {
+    patches: [
+      {
+        module: "MMM-Something",
+        method: "getDom", // Or Whatever method that module has.
+        patch: function (original, args) {
+          console.log('patch-start')
+          let ret = original(args) // Or do something by yourself
+          console.log('patch-end')
+          return ret // return expected return of origianl method.
+        }
+      }, // You can add more patches.
+    ]
+  }
+```
+This will replace `original` method of the module with `patch` function.
+
+## Why?
+When you need some modification of existing modules, but do not want to edit the original source code to prevent conflicts on upgrade/update.
+
+But.. I think rare people will use this module. :D. This was my half-day weekend killing-time project.
